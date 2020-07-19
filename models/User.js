4019595a5187b113b94461ba7368e95093d4fe69
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-07-19 15:50:55 -0500

    user model and index js file for my models

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const moment = require('moment');
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        }
+    ]
+},
+{
+
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    
+});
+
+// virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+module.export = User;
